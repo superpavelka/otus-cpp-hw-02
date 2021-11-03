2e5fc0614227d6e1a681c8fc75b2bab91cdaa822
krilatiy <pos@list.ru>
2021-11-03 19:31:37 +0300

    one allocate then bad_alloc

--- allocator_lib/MyAllocator.h
+++ allocator_lib/MyAllocator.h
@@ -7,50 +7,84 @@
 template <typename T>
 struct MyAllocator {
 	using value_type = T;
-
+	using pointer = T*;
+	using const_pointer = const T*;
+	using reference = T&;
+	using const_reference = const T&;
 
 	MyAllocator();
-
+	~MyAllocator();
+	template<typename U>
+	struct rebind {
+		using other = MyAllocator<U>;
+	};
+	
 	template <typename U>
 	MyAllocator(const MyAllocator<U>&);
 
-	T* allocate (std::size_t n);
-	
-	void deallocate (T* p, std::size_t n);
+	T* allocate (std::size_t size);	
+	void deallocate (T* p, std::size_t size);
+
+private:
+	T* p_start = nullptr;
+	size_t offset = 0;
+	size_t total_size = 10;
 };
 
 template <typename T>
-MyAllocator<T>::MyAllocator() {
-
+MyAllocator<T>::MyAllocator() 
+{
+	std::cout << "Constructor of MyAllocator" << std::endl;	
 }
 
 template <typename T>
 template <typename U>
-MyAllocator<T>::MyAllocator(const MyAllocator<U>& ) {
+MyAllocator<T>::MyAllocator(const MyAllocator<U>& ) 
+{
 	// should we make a copy of the rhs.m_buffer ?
 	// No, we should not!
+	std::cout << "Constructor of MyAllocator T U" << std::endl;
 }
 
 template <typename T>
-T* MyAllocator<T>::allocate (std::size_t n) {
+T* MyAllocator<T>::allocate(std::size_t count)
+{
+	if(!p_start)
+		p_start = (T*)malloc(total_size * sizeof(T));
+	if (!p_start)
+		throw std::bad_alloc();	
 
-	auto p = std::malloc(n * sizeof(T));
-	if (!p)
+	if (offset + count > total_size) 
+	{
 		throw std::bad_alloc();
-	return reinterpret_cast<T *>(p);
+	}
+
+	T* currentAddress = p_start + offset;
+	offset += count;
+
+	return reinterpret_cast<T*>(currentAddress);
+}
+
+template<class T> MyAllocator<T>::~MyAllocator() 
+{
+	std::cout << "Destructor of Allocator" << std::endl;
 }
 
 template <typename T>
-void MyAllocator<T>::deallocate (T* p, std::size_t ) {
-	std::free(p);
+void MyAllocator<T>::deallocate (T* p, std::size_t ) 
+{
+	free(p_start);
+	p_start = nullptr;
 }
 
 template <class T, class U>
-constexpr bool operator== (const MyAllocator<T>&, const MyAllocator<U>&) noexcept {
+constexpr bool operator== (const MyAllocator<T>&, const MyAllocator<U>&) noexcept 
+{
 	return false;
 }
 
 template <class T, class U>
-constexpr bool operator!= (const MyAllocator<T>&, const MyAllocator<U>&) noexcept {
+constexpr bool operator!= (const MyAllocator<T>&, const MyAllocator<U>&) noexcept 
+{
 	return false;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,15 @@ using namespace std;
 
 int main(int, char *[]) 
 {
-    
-	auto m = std::map<int, int,	std::less<int>,	MyAllocator<std::pair<const int, int>>>{};
-	
+	auto s = sizeof(int);
+	auto v = std::vector<int, MyAllocator<int>>{};
+	v.push_back(1);
+	auto m = std::map<int, float, std::less<int>, MyAllocator<std::pair<const int, float>>>{};
+	//bool tr = std::is_trivially_copyable<std::map<int, float, std::less<int>, MyAllocator<std::pair<const int, float>>>>::value;
+	m[0] = 1;
+	for (int i = 1; i < 10; ++i) 
+	{
+		m[i] = m[i - 1] * 2;
+	}
     return 0;
 }
