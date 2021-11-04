caf86a237a677fac9c7e726d769550b042500a94
krilatiy <pos@list.ru>
2021-11-04 15:49:06 +0300

    dynamically expanding doubly linked list allocator

--- allocator_lib/MyAllocator.h
+++ allocator_lib/MyAllocator.h
@@ -4,7 +4,24 @@
 #include <memory>
 #include <vector>
 
-template <typename T, size_t total_size = 10>
+class Chunk {
+public:
+	Chunk(std::size_t len) : total_size(len) {};
+	~Chunk();
+
+	void* node = nullptr;
+	Chunk* next = nullptr;
+	Chunk* prev = nullptr;
+	size_t total_size;
+};
+
+Chunk::~Chunk()
+{
+	free(node);
+	node = nullptr;
+};
+
+template <typename T>
 struct MyAllocator {
 	using value_type = T;
 	using pointer = T*;
@@ -12,74 +29,86 @@ struct MyAllocator {
 	using reference = T&;
 	using const_reference = const T&;
 
-	template <typename U> struct rebind {
-		typedef MyAllocator<U> other;
+	MyAllocator();
+	~MyAllocator();
+	template<typename U>
+	struct rebind {
+		using other = MyAllocator<U>;
 	};
 
-	MyAllocator() noexcept;
-	MyAllocator(const MyAllocator& myAllocator) noexcept;
-	MyAllocator(MyAllocator&& myAllocator) noexcept;
-	template <class U> MyAllocator(const MyAllocator<U>& myAllocator) noexcept;
-
-	~MyAllocator() noexcept;
+	template <typename U>
+	MyAllocator(const MyAllocator<U>&);
 
 	T* allocate(std::size_t size);
 	void deallocate(T* p, std::size_t size);
 
 private:
-	T* p_start = nullptr;
+	Chunk* mem_chunk = nullptr;
 	size_t offset = 0;
-	//size_t total_size = 10;
+	size_t total_size = 10;
 };
 
-template <typename T, size_t total_size>
-MyAllocator<T, total_size>::MyAllocator() noexcept
+template <typename T>
+MyAllocator<T>::MyAllocator()
 {
-	std::cout << "Constructor of MyAllocator T" << std::endl;
+	std::cout << "Constructor of MyAllocator" << std::endl;
 }
 
-template <typename T, size_t total_size>
-MyAllocator<T, total_size>::MyAllocator(const MyAllocator& myAllocator) noexcept : MyAllocator() {}
-
-template <typename T, size_t total_size>
-MyAllocator<T, total_size>::MyAllocator(MyAllocator&& myAllocator) noexcept
+template <typename T>
+template <typename U>
+MyAllocator<T>::MyAllocator(const MyAllocator<U>&)
 {
-
+	// should we make a copy of the rhs.m_buffer ?
+	// No, we should not!
+	std::cout << "Constructor of MyAllocator T U" << std::endl;
 }
 
-template <typename T, size_t total_size>
-MyAllocator<T, total_size>::~MyAllocator() noexcept
+template <typename T>
+T* MyAllocator<T>::allocate(std::size_t count)
 {
-	std::cout << "Destructor of MyAllocator T" << std::endl;
-}
-
-template <typename T, size_t total_size> template<class U>
-MyAllocator<T, total_size>::MyAllocator(const MyAllocator<U>& myAllocator) noexcept : MyAllocator() {}
-
-template <typename T, size_t total_size>
-T* MyAllocator<T, total_size>::allocate(std::size_t count)
-{
-	if (!p_start)
-		p_start = (T*)malloc(total_size * sizeof(T));
-	if (!p_start)
+	if (!mem_chunk)
+	{
+		mem_chunk = new Chunk(total_size);
+		mem_chunk->node = malloc(total_size * sizeof(T));
+	}
+	if (!mem_chunk->node)
 		throw std::bad_alloc();
 
 	if (offset + count > total_size)
 	{
-		throw std::bad_alloc();
+		mem_chunk->next = new Chunk(total_size + total_size * 2);
+		mem_chunk->next->prev = mem_chunk;
+		mem_chunk = mem_chunk->next;
+		total_size = total_size + total_size * 2;
+		mem_chunk->node = malloc(total_size * sizeof(T));
+		offset = 0;
 	}
 
-	T* currentAddress = p_start + offset;
+	T* currentAddress = (T*)mem_chunk->node + offset;
 	offset += count;
 
 	return reinterpret_cast<T*>(currentAddress);
 }
 
-template <typename T, size_t total_size>
-void MyAllocator<T, total_size>::deallocate(T* p, std::size_t)
+template<class T> MyAllocator<T>::~MyAllocator()
 {
-	free(p_start);
-	p_start = nullptr;
+	std::cout << "Destructor of Allocator" << std::endl;
+}
+
+template <typename T>
+void MyAllocator<T>::deallocate(T* p, std::size_t)
+{
+	if (mem_chunk)
+	{
+		while (mem_chunk->prev != nullptr)
+		{
+			mem_chunk = mem_chunk->prev;
+			delete mem_chunk->next;
+			mem_chunk->next = nullptr;
+		}
+		delete mem_chunk;
+		mem_chunk = nullptr;
+	}
 }
 
 template <class T, class U>
